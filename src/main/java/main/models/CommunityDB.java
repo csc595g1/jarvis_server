bf0e1b98dc091cfc621c170eb945df6143a3cda4
ed-miller1 <miller.edwardg@gmail.com>
2016-03-10 21:36:44 -0600

    adding community classes to call via postman to instantiate tables2

--- src/main/java/main/models/CommunityDB.java
+++ src/main/java/main/models/CommunityDB.java
@@ -5,10 +5,153 @@
  */
 package main.models;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Ed
+ * 
+ * Table Structures:
+ * community_post_tbl: post_id, email, content, dttm
+ * community_reply_tbl: reply_id, post_id, email, content, dttm
+ * upvote_post_tbl: post_id, dttm, email
+ * upvote_reply_tbl: reply_id, email, dttm
  */
 public class CommunityDB {
+    
+    Connection connection;
+    //todo create separate class for user image stuff;
+    private final String TABLE_COMMUNITY_POST_TBL = "community_post_tbl";
+    private final String TABLE_COMMUNITY_REPLY_TBL = "community_reply_tbl";
+    private final String TABLE_UPVOTE_POST_TBL = "upvote_post_tbl";
+    private final String TABLE_UPVOTE_REPLY_TBL = "upvote_reply_tbl";
+    
+    
+    
+    
+    
+    
+    /**
+     * Build tables in following order:
+     * 1. community post
+     * 2. replies
+     * 3. upvote post
+     * 4. upvote reply
+     */
+    public CommunityDB(){
+        this.createCommunityPostTable();
+        this.createCommunityReplyTable();
+        this.createUpvotePostTable();
+        this.createUpvoteReplyTable();
+}
+    
+    private void createUpvoteReplyTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS "
+                + TABLE_UPVOTE_REPLY_TBL
+                + "(reply_id integer references community_reply_tbl(reply_id), "
+                + "email varchar(250) NOT NULL, "
+                + "dttm timestamp);";
+        try{
+            connection = getConnection();
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            connection.close();
+        }
+        catch(URISyntaxException e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+        catch(SQLException e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+    }
+    
+    private void createUpvotePostTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS "
+                + TABLE_UPVOTE_POST_TBL
+                + "(post_id integer references community_post_tbl(post_id), "
+                + "email varchar(250) NOT NULL, "
+                + "dttm timestamp);";
+        try{
+            connection = getConnection();
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            connection.close();
+        }
+        catch(URISyntaxException e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+        catch(SQLException e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+    }
+    
+    private void createCommunityReplyTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS "
+                + TABLE_COMMUNITY_REPLY_TBL 
+                + "(reply_id BIGSERIAL PRIMARY KEY, "
+                + "post_id integer references community_post_tbl(post_id), "
+                + "email varchar(250) NOT NULL, "
+                + "content VARCHAR(250) NOT NULL, "
+                + "dttm timestamp);";
+        try{
+            connection = getConnection();
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            connection.close();
+        }
+        catch(URISyntaxException e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+        catch(SQLException e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+    }
+    
+    private void createCommunityPostTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS "
+                + TABLE_COMMUNITY_POST_TBL 
+                + "(post_id BIGSERIAL PRIMARY KEY, "
+                + "email VARCHAR(250) NOT NULL, "
+                + "content VARCHAR(250) NOT NULL, "
+                + "dttm timestamp);";
+        
+        try{
+            connection = getConnection();
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            connection.close();
+        }
+        catch(URISyntaxException e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+        catch(SQLException e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+    }
+    
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
+        
+        return DriverManager.getConnection(dbUrl, username, password);
+       
+    }    
     
 }
--- src/main/java/main/services/CommunityService.java
+++ src/main/java/main/services/CommunityService.java
@@ -5,10 +5,54 @@
  */
 package main.services;
 
+import com.sun.jersey.api.json.JSONConfiguration;
+import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.TimeZone;
+import main.models.CommunityDB;
+
 /**
  *
  * @author Ed
  */
+@Path("/community")
 public class CommunityService {
     
+    @GET
+    @Path("/getPosts")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPosts(){
+        CommunityDB commDB = new CommunityDB();
+        
+        //just to instantiate everything for now...
+        System.out.println("building tables....");
+        return Response.status(Response.Status.PRECONDITION_FAILED).build();
+
+    }
 }
